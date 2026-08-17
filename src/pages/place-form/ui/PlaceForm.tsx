7f83b1694d059c87f5cb42721a7c2e040de7ab7b
ptk5758 <ptk725739@gmail.com>
2026-08-17 18:57:34 +0900

    svae 장소 등록 화면 1

--- src/pages/place-form/ui/PlaceForm.tsx
+++ src/pages/place-form/ui/PlaceForm.tsx
@@ -1,19 +1,48 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { RootStackParamList } from '../../../app'
+import MapView, { Marker } from 'react-native-maps'
 
 type PlaceFormProps = NativeStackScreenProps<RootStackParamList, 'placeForm'>
 
 function PlaceForm({ route }: PlaceFormProps) {
     const { coordinate } = route.params
     return (
-        <View>
-            <Text>장소 등록 & 수정</Text>
-            <Text>
-                {coordinate.latitude}
-                {coordinate.longitude}
-            </Text>
+        <View style={styles.page}>
+            <View style={styles.location}>
+                <MapView
+                    style={styles.locationMap}
+                    initialRegion={{
+                        ...coordinate,
+                        latitudeDelta: 0.005, // zoom 관련
+                        longitudeDelta: 0.005, // zoom 관련
+                    }}
+                    zoomEnabled={false}
+                    rotateEnabled={false}
+                    scrollEnabled={false}
+                    pitchEnabled={false}
+                >
+                    <Marker title="선택 한 장소" coordinate={coordinate} />
+                </MapView>
+            </View>
+            <View style={styles.content}>
+                <Text>Form 영역</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+    },
+    location: {
+        // flex: 1,
+        height: 200,
+    },
+    locationMap: {
+        flex: 1,
+    },
+    content: {},
+})
 export default PlaceForm
